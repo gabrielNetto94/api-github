85969dd406a8359e5f59ca8585ad5fc3256a32fa
gabrielNetto94 <silveiranetto94@gmail.com>
2021-01-13 16:54:08 -0300

    remoção de uma rota

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const fs = require('fs');
+
 const app = express();
 
 const teste = require('./teste');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-var numberFollowers;
 
 app.get('/user/:username', (req, res) => {
 
@@ -22,8 +21,6 @@ app.get('/user/:username', (req, res) => {
         .then(function (response) {
 
             const { name, followers, avatar_url } = response.data;
-
-            numberFollowers = followers;
 
             axios.get('https://api.github.com/users/' + username + '/followers', {})
                 .then(function (response) {
@@ -44,52 +41,29 @@ app.get('/user/:username', (req, res) => {
                         fs.unlinkSync(path + username + '.json');
                     }
 
-                    //stringfy  object to JSON
-                    //parse JSON to object
                     jsonFollowers = JSON.stringify(followersNames)
-                
-                    fs.writeFileSync(path + username + ".json", jsonFollowers, 'utf8', () => {
-                        console.log('Arquivo criado!');
-                    });
 
-                    var teste = JSON.parse(jsonFollowers);
+                    fs.writeFileSync(path + username + ".json", jsonFollowers, 'utf8', () => { });
 
                 }).catch(function (error) {
                     res.json({ error });
                 });
 
+                
+            var fileJson = fs.readFileSync('users/' + username + '.json');
+            var obj = JSON.parse(fileJson);
+
             res.json({
                 name,
+                avatar_url,
                 followers,
-                followersNames,
-                avatar_url
-
+                followersNames: obj
             });
         })
         .catch(function (error) {
             res.json({ error });
         });
-
-
 });
-
-app.get('/userfile/:username', (req, res) => {
-
-    const { username } = req.params;
-
-    var fileJson = fs.readFileSync('users/' + username + '.json');
-    
-    var obj = JSON.parse(fileJson);
-    
-    res.json({
-        username,
-        numberFollowers,
-        followers:obj
-    });
-
-});
-
-
 
 app.listen(3000);
 console.log("Server running on port 3000!")
