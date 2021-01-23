130b398b80277dae2981c6c43d231cc5e59344ba
gabrielNetto94 <silveiranetto94@gmail.com>
2021-01-23 15:31:15 -0300

    Teste de concatenação das notas com as disciplinas

--- testFile.js
+++ testFile.js
@@ -1,5 +1,65 @@
 const puppeteer = require('puppeteer');
 
+const disciplinas = [
+  "EDU250 - Antropologia e Cosmovisão Franciscana",
+  "CPT329 - Fundamentos de Computação Gráfica",
+  "CPT330 - Legislação em Informática",
+  "CPT332 - Qualidade de Software",
+  "CPT373 - Sistemas Cooperativos",
+  "CNT102 - Trabalho Final de Graduação I"
+];
+const nota_1_bi = [
+  "1,0",
+  "2,0",
+  "3,0",
+  "4,0",
+  "5,0",
+  "6,0"
+];
+const nota_2_bi = [
+  "1,0",
+  "2,0",
+  "3,0",
+  "4,0",
+  "5,0",
+  "6,0"
+];
+const nota_3_bi = [
+  "1,0",
+  "2,0",
+  "3,0",
+  "4,0",
+  "5,0",
+  "6,0"
+];
+
+
+const a = {
+  disciplina1: {
+    nota1: '8,7',
+    nota2: '9,4',
+    nota3: '8,4',
+  },
+  disciplina2: {
+    nota1: '8,7',
+    nota2: '9,4',
+    nota3: '8,4'
+  },
+}
+
+const tabelaNotas = {};
+for (i in disciplinas) {
+  tabelaNotas[i] = {
+    disciplina: disciplinas[i],
+    nota1: nota_1_bi[i],
+    nota2: nota_2_bi[i],
+    nota3: nota_2_bi[i]
+
+  }
+}
+console.table(tabelaNotas)
+
+/*
 (async () => {
 
   const browser = await puppeteer.launch();
@@ -28,7 +88,7 @@ const puppeteer = require('puppeteer');
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  
+
   await page.goto('https://br.investing.com/crypto/bitcoin/btc-brl', {
     "waitUntil": "networkidle0"
   });
@@ -47,4 +107,4 @@ const puppeteer = require('puppeteer');
   await browser.close();
 })();
 
-
+*/
